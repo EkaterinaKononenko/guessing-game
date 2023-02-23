15ff320a6bd2fc2b6ff4552f304d4fea3fe5a7f8
EkaterinaKononenko <kaktysik4@ukr.net>
2023-02-23 09:57:06 +0200

    task is done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = null;
+    this.max = null;
+    this.number = null;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.number = Math.round(Math.ceil(this.min + this.max) / 2);
+    return this.number;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.number;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.number;
+  }
 }
 
 module.exports = GuessingGame;
